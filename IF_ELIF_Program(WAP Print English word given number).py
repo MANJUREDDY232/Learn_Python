ad9dff8cf3b9768a32e086a4cd4467a9f55f3fb8
Manjunatha Reddy <manjueee232@gmail.com>
2025-08-20 22:53:09 +0530

    Intital_program

---
+++ IF_ELIF_Program(WAP Print English word given number).py
@@ -0,0 +1,26 @@
+Number=int(input("Enter a Number from 0 to 9:"))
+num=10
+for Number in num:
+ if Number == 0:
+  output='Zero'
+ elif Number == 1:
+  output='One'
+ elif Number == 2:
+  output='Two'
+ elif Number == 3:
+  output='Three'
+ elif Number == 4:
+  output='Four'
+ elif Number == 5:
+  output='Five'
+ elif Number == 6:
+  output='Six'
+ elif Number == 7:
+  output='Seven'
+ elif Number == 8:
+  output='Eight'
+ elif Number == 9:
+  output='Nine'
+ else:
+  output='Enter number from 0 to 9 only'
+print(Number,':',output)
